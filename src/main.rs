39ca4c5bbc87493fc8498cee1b5962c10edc0fdf
Krak9n <alyoshagnatu@gmail.com>
2025-08-22 17:42:43 +0200

    as of now im done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::process::Output;
 use colored::Colorize;
-use wmctrl::*;
+// use wmctrl::*;
+// was needed for wm.
+// maybe will use in the future
+// fuck it
 use sysinfo::{
     Components, Disks, Networks, System, RefreshKind, CpuRefreshKind,
 };
+use std::process::Command;
 
 // to me tmrw
 // finish this shit as u planned the first time
@@ -14,10 +18,10 @@ fn main() {
 
   // this sucks and i know that it does
   // probably will change in the future
+  let block = String::from("\u{2588}");
   let left = String::from("\u{250F}");
   let right = String::from("\u{2513}");
   let hor = String::from("\u{2501}");
-  let ver = String::from("\u{2503}");
   let left_bot = String::from("\u{2517}");
   let right_bot = String::from("\u{251B}");
 
@@ -25,11 +29,11 @@ fn main() {
   let width = sisi.cpu.clone().chars().count();
 
   // top
-  println!("\t{}{}{}", left, hor.repeat(width), right);
+  println!("{}{}{}", left, hor.repeat(width+10), right);
   // modules and os name
-  FETCH::make_logo(ver.clone(), sisi.os.clone(), width);
+  FETCH::make_logo(block);
   // bottom 
-  println!("\t{}{}{}", left_bot, hor.repeat(width), right_bot);
+  println!("{}{}{}", left_bot, hor.repeat(width+10), right_bot);
 
 }
 
@@ -37,7 +41,7 @@ pub fn colors(text: &str, field: &Option<String>, color: &str) {
   if let Some(texts) = field {
     #[cfg(feature = "field-titles")]
     print!("{} ", text.bright_white());
-    print!(
+    println!(
       "{}",
       format!("{}{}", text, texts).color(color)
     );
@@ -48,10 +52,11 @@ pub fn colors(text: &str, field: &Option<String>, color: &str) {
 struct FETCH {
   os: Option<String>,
   kernel: String,
-  //gpu: &'a str,
+  gpu: Option<Vec<String>>,
   cpu: String,
   memory: String,
-  wm: Output,
+  //wm: String,
+  user: String,
 }
 
 impl FETCH {
@@ -63,12 +68,34 @@ impl FETCH {
     Self {
       os: System::name(),
       kernel: System::kernel_long_version().clone(),
-      memory: (sys.total_memory() / 1_000).to_string(), // later make it in function for printing
-                                  // in mbits
-      wm: wmctrl::show_wm_information(), // later make some loop
-                                         // for checking if x11 or
-                                         // wayland
-      // later gpu: ,
+      memory: 
+        format!("{} gb / {} gb", 
+          (sys.total_memory() / 1_000 / 1_000 / 1_000), (sys.used_memory() / 1_000 / 1_000 / 1_000))
+        .to_string(), 
+     // wm: {
+       // let output = Command::new("sh")
+        //  .args(["-c", ""
+      //},
+
+      //to find the gpu:
+      // lspci | grep VGA | awk -F'VGA compatible controller: ' '{print $2}' 
+      gpu: {
+        let output = Command::new("sh")
+          .arg("-c")
+          .arg("lspci | grep VGA | awk -F'VGA compatible controller: ' '{print $2}'")
+          .output()
+          .expect("failed to execute process");
+        
+        let returning: String = String::from_utf8_lossy(&output.stdout).to_string();
+        let mut gpu_vec = vec![];
+        for s in returning.trim().split('\n') {
+            gpu_vec.push(s.to_string());
+        }
+        
+        Some(gpu_vec)
+      },
+
+      user: whoami::username(), 
       cpu: String::from(sys.cpus()[0].brand()).clone(),
     }
   }
@@ -78,32 +105,36 @@ impl FETCH {
   // and width by c (char) + cpu 
   fn just_modules() {
     let sisi = Self::new();
-    //colors_non_std("memory: ", &sisi.wm, "magenta"); 
-    //for mut modules in 0..3 {
-    colors("cpu: ", &Some(sisi.cpu.clone()), "magenta"); 
-    //colors("memory: ", &Some(sisi.memory.clone()), "magenta"); 
-    //colors("kernel: ", &Some(sisi.kernel.clone()), "magenta");
-    //modules +=1;
-    //}
+
+    // name of the user
+    colors("  user: ", &Some(sisi.user.clone()), "purple");     
+    colors("  os: ", &Some(sisi.os.expect("reason").clone()), "green");
+    colors("  cpu: ", &Some(sisi.cpu.clone()), "red"); 
+
+    if let Some(gpuvec) = sisi.gpu {
+        for gpu in gpuvec {
+          colors("  gpu: ", &Some(gpu), "yellow");
+        }
+    }
+    colors("  memory: ", &Some(sisi.memory.clone()), "white"); 
+    colors("  kernel: ", &Some(sisi.kernel.clone()), "blue");
+    //colors_non_std("  wm: ", &Some(sisi.wm.clone()), "magenta");
+    //
   }
 
-  fn make_logo(ver: String, s: Option<String>, count: usize) {
-    // colors("", &sys.os, "green");
-    let cp: String = s.expect("REASON").to_string();
+  fn make_logo(block: String) {
 
-    let mut hasnt: bool = false;
-    for c in cp.chars() {
-      print!("\t{}", ver);
-      print!(" {} ", c);
-
-      if hasnt == false {
-        Self::just_modules();
-      }  
-      
-      print!("{: <1$}", "", count);
-      println!("{}", ver);
-      hasnt = true;
-    }
+    Self::just_modules();
+    println!();
+    
+    println!("  {}{}{}{}{}{}{}{}{}{}{}{}", 
+      block.blue(), block.blue(), 
+      block.black(), block.black(), 
+      block.truecolor(0, 247, 255), block.truecolor(0, 247, 255),
+      block.truecolor(255, 0, 50), block.truecolor(255, 0, 50),
+      block.truecolor(168, 0, 33), block.truecolor(168, 0, 33),
+      block.purple(), block.purple()
+      ); 
   }
 
 }
